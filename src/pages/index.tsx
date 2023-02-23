b9306b267185c931445bfc05247d3ba7874ef0b9
Alejandro Valencia <alejandro.d.valencia@gmail.com>
2023-02-23 15:33:28 -0300

    feat: first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,104 @@
+import Image from 'next/image';
 import * as React from 'react';
+import ReactPlayer from 'react-player/lazy';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 import Seo from '@/components/Seo';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
+// https://nextjs.org/docs/api-reference/next/image
 
 export default function HomePage() {
+  const [hasWindow, setHasWindow] = React.useState(false);
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHasWindow(true);
+    }
+  }, []);
+
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
-      <Seo />
+      <Seo templateTitle='Home' />
 
       <main>
         <section className='bg-white'>
-          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
+          <h1>Videos gallery</h1>
 
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
+          {hasWindow && (
+            <div className='container mx-auto flex flex-col items-center'>
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
               />
-            </UnstyledLink>
-
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
-            </footer>
-          </div>
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <Image
+                src='https://images.pexels.com/photos/14683268/pexels-photo-14683268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                alt='some-image'
+                width={500}
+                height={500}
+              />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+              <ReactPlayer url='https://www.youtube.com/watch?v=9t9Ym8CjnDI&ab_channel=StarrySky' />
+            </div>
+          )}
         </section>
       </main>
     </Layout>
