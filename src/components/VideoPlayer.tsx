39e7c20b7d901c70858a3a7a3baad3c64161f998
Alejandro <alejandro.d.valencia@gmail.com>
2023-02-25 22:40:12 -0300

    feat: test mobile

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,10 +1,13 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import handleViewport from 'react-in-viewport';
 import ReactPlayer from 'react-player/youtube';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const VideoPlayer = memo((props: any) => {
   const { inViewport, forwardedRef, playing } = props;
+  useEffect(() => {
+    alert(`inViewport: ${inViewport}, will play ${playing && inViewport}`);
+  }, [inViewport, playing]);
   return (
     <div
       className='relative mt-10 pt-[56.25%] transition-opacity'
