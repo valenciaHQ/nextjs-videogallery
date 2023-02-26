2f9a4c82bb2f3173b89714e03af98811000a6c91
Alejandro <alejandro.d.valencia@gmail.com>
2023-02-26 01:25:28 -0300

    feat: testing

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,45 +1,42 @@
-import { memo, useEffect, useRef, useState } from 'react';
-import { useInViewport } from 'react-in-viewport';
+import { memo, useState } from 'react';
 import ReactPlayer from 'react-player/lazy';
+import { Waypoint } from 'react-waypoint';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const VideoPlayer = memo((props: any) => {
-  const myRef: React.RefObject<HTMLElement> = useRef(null);
-  const { inViewport } = useInViewport(myRef, props);
+const VideoPlayer = memo(() => {
   const [playing, setPlaying] = useState(false);
 
-  useEffect(() => {
-    if (inViewport) {
-      setPlaying(true);
-    }
-  }, [inViewport]);
+  const handleEnterViewport = function () {
+    setPlaying(true);
+  };
+  const handleExitViewport = function () {
+    setPlaying(false);
+  };
 
   return (
-    <div
-      className='relative mt-10 pt-[56.25%] transition-opacity'
-      ref={myRef as React.RefObject<HTMLDivElement>}
-    >
-      <ReactPlayer
-        style={{
-          position: 'absolute',
-          top: '0',
-          left: '0',
-        }}
-        url='https://www.youtube.com/watch?v=1g7TrcIlpMk&ab_channel=OliverAstrologo'
-        width='100%'
-        height='100%'
-        playing={playing}
-        playsInline
-        fallback={<p>Loading</p>}
-        config={{
-          youtube: {
-            playerVars: { origin: 'https://www.youtube.com' },
-            onUnstarted: () => inViewport && setPlaying(true),
-          },
-        }}
-        on
-      />
-    </div>
+    <Waypoint onEnter={handleEnterViewport} onLeave={handleExitViewport}>
+      <div className='relative mt-10 pt-[56.25%] transition-opacity'>
+        <ReactPlayer
+          style={{
+            position: 'absolute',
+            top: '0',
+            left: '0',
+          }}
+          url='https://www.youtube.com/watch?v=1g7TrcIlpMk&ab_channel=OliverAstrologo'
+          width='100%'
+          height='100%'
+          playing={playing}
+          playsInline={true}
+          fallback={<p>Loading</p>}
+          config={{
+            youtube: {
+              playerVars: { origin: 'https://www.youtube.com' },
+            },
+          }}
+          on
+        />
+      </div>
+    </Waypoint>
   );
 });
 
