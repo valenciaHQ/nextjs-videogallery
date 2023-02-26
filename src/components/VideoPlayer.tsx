597e11f3011f0fadc8f98e73cb01d275c6683ae9
Alejandro <alejandro.d.valencia@gmail.com>
2023-02-25 21:54:10 -0300

    feat: changed viewport tracker

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,32 +1,32 @@
-import { memo, useState } from 'react';
+import handleViewport from 'react-in-viewport';
 import ReactPlayer from 'react-player/youtube';
-import { Waypoint } from 'react-waypoint';
 
-const VideoPlayer = memo(() => {
-  const [playing, setPlaying] = useState(false);
-
+const VideoPlayer = (props: any) => {
+  const { inViewport, forwardedRef } = props;
   return (
-    <Waypoint
-      onEnter={() => setPlaying(true)}
-      onLeave={() => setPlaying(false)}
-      debug
+    <div
+      className='relative mt-10 pt-[56.25%] transition-opacity'
+      ref={forwardedRef}
     >
-      <div className='relative mt-10 pt-[56.25%] transition-opacity'>
-        <ReactPlayer
-          style={{
-            position: 'absolute',
-            top: '0',
-            left: '0',
-          }}
-          url='https://www.youtube.com/watch?v=1g7TrcIlpMk&ab_channel=OliverAstrologo'
-          width='100%'
-          height='100%'
-          playing={playing}
-          fallback={<p>Loading</p>}
-        />
-      </div>
-    </Waypoint>
+      <ReactPlayer
+        style={{
+          position: 'absolute',
+          top: '0',
+          left: '0',
+        }}
+        url='https://www.youtube.com/watch?v=1g7TrcIlpMk&ab_channel=OliverAstrologo'
+        width='100%'
+        height='100%'
+        playing={inViewport}
+        fallback={<p>Loading</p>}
+      />
+    </div>
   );
-});
+};
 
-export default VideoPlayer;
+export { VideoPlayer };
+
+const ViewportVideoPlayer = handleViewport(
+  VideoPlayer /** options: {}, config: {} **/
+);
+export default ViewportVideoPlayer;
